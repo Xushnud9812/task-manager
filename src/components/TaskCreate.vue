ac986da1b806b9212cd7df354a8fb060071718b2
Xushnudbek <hushnud1208@mail.ru>
2023-08-02 15:58:25 +0500

    fix issues

--- src/components/TaskCreate.vue
+++ src/components/TaskCreate.vue
@@ -61,92 +61,96 @@ const createTask = () => {
               <span class="text-2xl">&times;</span>
             </button>
           </div>
-          <div class="grid grid-cols-2 gap-4">
-            <div class="col-span-2">
-              <label for="input1">Title</label>
-              <input
-                v-model="newTask.title"
-                type="text"
-                placeholder="Title"
-                class="w-full px-3 py-2 border focus:outline-none focus:border-primary rounded-lg"
-              />
-            </div>
-            <div>
-              <label for="">Date</label>
-              <input
-                v-model="newTask.date"
-                type="date"
-                placeholder="Input 2"
-                class="w-full px-3 py-2 border focus:outline-none focus:border-primary rounded-lg"
-              />
-            </div>
-            <div class="relative w-full">
-              <label for="">Type</label>
-              <Listbox v-model="newTask.taskType">
-                <ListboxButton
-                  class="relative bg-white w-full text-left px-3 py-2 border focus:outline-none focus:border-primary rounded-lg"
-                >
-                  <span class="block">
-                    {{ newTask.taskType }}
-                  </span>
-                  <span
-                    class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2"
+          <form action="" @submit="createTask">
+            <div class="grid grid-cols-2 gap-4">
+              <div class="col-span-2">
+                <label for="input1">Title</label>
+                <input
+                  v-model="newTask.title"
+                  required
+                  type="text"
+                  placeholder="Title"
+                  class="w-full px-3 py-2 border focus:outline-none focus:border-primary rounded-lg"
+                />
+              </div>
+              <div>
+                <label for="">Date</label>
+                <input
+                  v-model="newTask.date"
+                  type="date"
+                  required
+                  placeholder="Input 2"
+                  class="w-full px-3 py-2 border focus:outline-none focus:border-primary rounded-lg"
+                />
+              </div>
+              <div class="relative w-full">
+                <label for="">Type</label>
+                <Listbox v-model="newTask.taskType">
+                  <ListboxButton
+                    class="relative bg-white w-full text-left px-3 py-2 border focus:outline-none focus:border-primary rounded-lg"
                   >
-                    <ChevronUpDownIcon
-                      class="h-5 w-5 text-gray-400"
-                      aria-hidden="true"
-                    />
-                  </span>
-                </ListboxButton>
-                <transition
-                  leave-active-class="transition duration-100 ease-in"
-                  leave-from-class="opacity-100"
-                  leave-to-class="opacity-0"
-                >
-                  <ListboxOptions
-                    class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
-                  >
-                    <ListboxOption
-                      v-slot="{ active, selected }"
-                      v-for="(type, index) in taskTypes"
-                      :key="index"
-                      :value="type"
-                      as="template"
+                    <span class="block">
+                      {{ newTask.taskType }}
+                    </span>
+                    <span
+                      class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2"
                     >
-                      <li
-                        :class="[
-                          active ? 'bg-primary ' : 'text-gray-900',
-                          'relative cursor-default select-none py-2 pl-10 pr-4',
-                        ]"
+                      <ChevronUpDownIcon
+                        class="h-5 w-5 text-gray-400"
+                        aria-hidden="true"
+                      />
+                    </span>
+                  </ListboxButton>
+                  <transition
+                    leave-active-class="transition duration-100 ease-in"
+                    leave-from-class="opacity-100"
+                    leave-to-class="opacity-0"
+                  >
+                    <ListboxOptions
+                      class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+                    >
+                      <ListboxOption
+                        v-slot="{ active, selected }"
+                        v-for="(type, index) in taskTypes"
+                        :key="index"
+                        :value="type"
+                        as="template"
                       >
-                        <span
+                        <li
                           :class="[
-                            selected ? 'font-medium' : 'font-normal',
-                            'block truncate',
+                            active ? 'bg-primary ' : 'text-gray-900',
+                            'relative cursor-default select-none py-2 pl-10 pr-4',
                           ]"
-                          >{{ type }}</span
                         >
-                        <span
-                          v-if="selected"
-                          class="absolute inset-y-0 left-0 flex items-center pl-3"
-                        >
-                          <CheckIcon class="h-5 w-5" aria-hidden="true" />
-                        </span>
-                      </li>
-                    </ListboxOption>
-                  </ListboxOptions>
-                </transition>
-              </Listbox>
+                          <span
+                            :class="[
+                              selected ? 'font-medium' : 'font-normal',
+                              'block truncate',
+                            ]"
+                            >{{ type }}</span
+                          >
+                          <span
+                            v-if="selected"
+                            class="absolute inset-y-0 left-0 flex items-center pl-3"
+                          >
+                            <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        </li>
+                      </ListboxOption>
+                    </ListboxOptions>
+                  </transition>
+                </Listbox>
+              </div>
             </div>
-          </div>
-          <div class="flex justify-end pt-4">
-            <button
-              @click="createTask"
-              class="px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600"
-            >
-              Create
-            </button>
-          </div>
+            <div class="flex justify-end pt-4">
+              <button
+                type="submit"
+                class="px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600"
+              >
+                Create
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
